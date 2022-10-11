69212f52ec0997a6ec4c601bbb6d49abb46ac9a4
Michael <chavom68@gmail.com>
2022-10-11 12:00:51 -0700

    Event operations and streams

---
+++ node/tutorial/event-loop-examples/read-file.js
@@ -0,0 +1,14 @@
+const {readFile} = require('fs')
+
+console.log('started first task')
+
+//Check file path
+readFile('./contents/first.txt', 'utf-8', (err,result)=> {
+	if(err) {
+		console.log(err)
+		return
+	}
+	console.log(result)
+	console.log('completed first task')
+})
+console.log('starting next task')
